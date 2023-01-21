715b6dc7efd8152c1e8494a1e582d2083adc93cd
nemoVim <nemovim@kentech.ac.kr>
2023-01-21 19:41:35 +0900

    Restruct client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, resolveBaseUrl } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: 'index.html',
+        admin: 'admin.html',
+      }
+    }
+  }
 })
